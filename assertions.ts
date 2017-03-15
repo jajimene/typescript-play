25e062c09e187cbc5ba113d0a108f65804878dee
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2017-03-15 16:57:51 +0100

    Assertions sample.

---
+++ assertions.ts
@@ -0,0 +1,5 @@
+let someValue: any = "this is a string";
+// angle-bracket syntax
+let strLength: number = (<string>someValue).length;
+// as syntax
+let strLengthB: number = (someValue as string).length;
