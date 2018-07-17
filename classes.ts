e441597d9ecf2420f8c8fbaad38cc6d343d14e84
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-07-17 10:47:31 +0200

    Minor updates and improvements related to new TypeScript version

--- classes.ts
+++ classes.ts
@@ -1,43 +1,49 @@
 class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return 'Hello, ' + this.greeting;
+  }
 }
 
-let greeter = new Greeter("world");
+let greeter = new Greeter('world');
 
 ///////////////////////////////////////////////////////////////////////////
 
 class Animal {
-    name: string;
-    constructor(theName: string) { this.name = theName; }
-    move(distanceInMeters: number = 0) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
+  name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
 }
 
 class Snake extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 5) {
-        console.log("Slithering...");
-        super.move(distanceInMeters);
-    }
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 5) {
+    console.log('Slithering...');
+    super.move(distanceInMeters);
+  }
 }
 
 class Horse extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 45) {
-        console.log("Galloping...");
-        super.move(distanceInMeters);
-    }
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 45) {
+    console.log('Galloping...');
+    super.move(distanceInMeters);
+  }
 }
 
-let sam = new Snake("Sammy the Python");
-let tom: Animal = new Horse("Tommy the Palomino");
+let sam = new Snake('Sammy the Python');
+let tom: Animal = new Horse('Tommy the Palomino');
 
 sam.move();
 tom.move(34);
@@ -45,50 +51,55 @@ tom.move(34);
 ///////////////////////////////////////////////////////////////////////////
 
 class Person {
-    protected name: string;
-    constructor(name: string) { this.name = name; }
+  protected name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 class Employee extends Person {
-    private department: string;
+  private department: string;
 
-    constructor(name: string, department: string) {
-        super(name);
-        this.department = department;
-    }
+  constructor(name: string, department: string) {
+    super(name);
+    this.department = department;
+  }
 
-    public getElevatorPitch() {
-        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
-    }
+  public getElevatorPitch() {
+    return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+  }
 }
 
-let howard = new Employee("Howard", "Sales");
+let howard = new Employee('Howard', 'Sales');
 console.log(howard.getElevatorPitch());
 console.log(howard.name); // error
 
 ///////////////////////////////////////////////////////////////////////////
 
-let passcode = "secret passcode 2";
+let passcode = 'secret passcode 2';
 
 class Employee2 {
-    private _fullName: string;
+  private _fullName: string;
 
-    get fullName(): string {
-        return this._fullName;
-    }
+  constructor(_fullname: string) {
+    this._fullName = _fullname;
+  }
 
-    set fullName(newName: string) {
-        if (passcode && passcode == "secret passcode") {
-            this._fullName = newName;
-        }
-        else {
-            console.log("Error: Unauthorized update of employee!");
-        }
+  get fullName(): string {
+    return this._fullName;
+  }
+
+  set fullName(newName: string) {
+    if (passcode && passcode == 'secret passcode') {
+      this._fullName = newName;
+    } else {
+      console.log('Error: Unauthorized update of employee!');
     }
+  }
 }
 
-let employee = new Employee2();
-employee.fullName = "Bob Smith";
+let employee = new Employee2('Bad Name');
+employee.fullName = 'Bob Smith';
 if (employee.fullName) {
-    console.log(employee.fullName);
+  console.log(employee.fullName);
 }
